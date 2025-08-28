477fc544bd33399daacdaf2df821ac9c529fb67d
Riya-676 <riyavtechnocomet@gmail.com>
2025-08-28 19:14:41 +0530

    dashboard and navbar component

--- src/Sections/Dashboard/Dashboard.js
+++ src/Sections/Dashboard/Dashboard.js
@@ -1,14 +1,22 @@
-import Image from 'next/image'
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import Image from "next/image";
+import React from "react";
+import Navbar from "../Navbar/Navbar";
 export default function Dashboard() {
   return (
     <>
       <div>
-        <div className='w-full h-[100vh] dashboard-bg-img'>
-            <Navbar/>
+        <div className="w-full h-[100vh] dashboard-bg-img text-white">
+          <Navbar />
+          <div>
+            <div className="text-[35px] w-[28%] font-[600] leading-[44px] family-TwCenMT">
+              When You Would Like To Go On A Date?
+            </div>
+            <div className="text-[13px] font-[400] mt-[1.5rem]">
+              Application aimed at singles looking for a relationship.
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
--- src/Sections/Navbar/Navbar.js
+++ src/Sections/Navbar/Navbar.js
@@ -1,113 +1,173 @@
-'use client';
+"use client";
 import Image from "next/image";
 import React, { useState } from "react";
 import hukuplogo from "../../assets/images/HukupLogo.png";
 import appstore from "../../assets/images/appstore.png";
 import playstore from "../../assets/images/playstore.png";
-
+import { RxHamburgerMenu } from "react-icons/rx";
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <div className="ml-[1rem] mr-[1rem] py-[1.5rem] flex items-center justify-between">
+      <div className="ml-[1rem] lg:ml-[3rem] mr-[1rem] py-[1.5rem] flex items-center justify-between">
         {/* Logo */}
         <div>
           <Image
             src={hukuplogo}
             alt="HukupLogo"
-            className="w-[200px] h-[57px]"
+            className="lg:w-[200px] lg:h-[57px] w-[140px] h-[40px]"
           />
         </div>
-
-        {/* Desktop Menu (Visible only on lg and above) */}
-        <div className="hidden lg:flex text-white text-[14px] font-[500]">
+        {/* navigation */}
+        <div className="hidden lg:flex text-white text-[14px] family-reemkufi xl:text-[16px] 2xl:text-[18px] font-[500]">
           <ul className="flex gap-[1.5rem] cursor-pointer">
-            <a className="text-[#EF3672]">Home</a>
+            <a className="lg:text-[#EF3672] text-white">Home</a>
             <select
               name="activity"
               id="activity"
-              className="hover:text-[#EF3672] text-white outline-none bg-transparent"
+              className="hover:text-[#EF3672] text-white outline-none "
             >
-              <option value="activity" className="text-black">Your Activity</option>
-              <option value="activity" className="text-black">Saab</option>
-              <option value="activity" className="text-black">Mercedes</option>
-              <option value="activity" className="text-black">Audi</option>
+              <option value="activity" className="text-white bg-black">
+                Your Activity
+              </option>
+              <option value="activity" className="text-white bg-black">
+                Saab
+              </option>
+              <option value="activity" className="text-white bg-black">
+                Mercedes
+              </option>
+              <option value="activity" className="text-white bg-black">
+                Audi
+              </option>
             </select>
             <select
               name="saved"
               id="saved"
               className="hover:text-[#EF3672] text-white outline-none bg-transparent"
             >
-              <option value="saved" className="text-black">Saved</option>
-              <option value="saved" className="text-black">Saab</option>
-              <option value="saved" className="text-black">Mercedes</option>
-              <option value="saved" className="text-black">Audi</option>
+              <option value="activity" className="text-white bg-black">
+                Saved
+              </option>
+              <option value="activity" className="text-white bg-black">
+                Saab
+              </option>
+              <option value="activity" className="text-white bg-black">
+                Mercedes
+              </option>
+              <option value="activity" className="text-white bg-black">
+                Audi
+              </option>
             </select>
             <a className="hover:text-[#EF3672]">Favorites</a>
             <a className="hover:text-[#EF3672]">Group Profiles</a>
           </ul>
         </div>
 
-        {/* Desktop Get App Button (lg and above) */}
+        {/* offcanvas */}
         <div className="hidden lg:block p-[4px] rounded-4xl bg-gradient-to-br from-[rgba(252,114,146,0.4)] to-[rgba(238,237,237,0.2)] cursor-pointer">
-          <div className="text-[13px] font-[500] rounded-4xl bg-gradient-to-br from-[rgba(252,114,146,0.4)] to-[rgba(238,237,237,0.2)] py-[9px] px-[10px] text-white flex items-center gap-[7px]">
+          <div className="text-[14px] xl:text-[16px] 2xl:text-[18px] font-[500] rounded-4xl bg-gradient-to-br from-[rgba(252,114,146,0.4)] to-[rgba(238,237,237,0.2)] py-[9px] px-[10px] text-white flex items-center gap-[7px]">
             Get App
             <div className="flex items-center gap-[7px]">
-              <Image src={appstore} alt="appstorelink" className="w-[12px] h-[15px]" />
-              <Image src={playstore} alt="playstorelink" className="w-[12px] h-[15px]" />
+              <Image
+                src={appstore}
+                alt="appstorelink"
+                className="lg:w-[12px] lg:h-[15px] xl:w-[13px] xl:h-[18px]"
+              />
+              <Image
+                src={playstore}
+                alt="playstorelink"
+                className="lg:w-[12px] lg:h-[15px] xl:w-[13px] xl:h-[18px]"
+              />
             </div>
           </div>
         </div>
-
-        {/* Mobile/Tablet Menu Button (hidden on lg and above) */}
         <button
           onClick={() => setIsOpen(true)}
-          className="lg:hidden text-white px-3 py-2 border border-white rounded"
+          className="lg:hidden text-white md:px-2 md:py-1 p-1 border border-white rounded"
         >
-          ☰
+          <RxHamburgerMenu className="md:w-[32px] md:h-[32px] w-[20px] h-[20px]" />
         </button>
       </div>
-
-      {/* Offcanvas Drawer (only for < lg screens) */}
       <div
-        className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 
+        className={`fixed top-0 right-0 h-full w-64 bg-black text-white-500 transform transition-transform duration-300 ease-in-out z-50 
         ${isOpen ? "translate-x-0" : "translate-x-full"} lg:hidden`}
       >
         <div className="p-4 flex justify-between items-center border-b">
-          <h2 className="text-lg font-semibold">Menu</h2>
+          <div className="">
+            <Image
+              src={hukuplogo}
+              alt="HukupLogo"
+              className=" w-[140px] h-[40px]"
+            />
+          </div>
           <button
             onClick={() => setIsOpen(false)}
-            className="text-black px-2 py-1 border rounded"
+            className="text-white px-2 py-1 "
           >
             ✕
           </button>
         </div>
-
-        <ul className="flex flex-col gap-4 p-4 text-black">
+        <ul className="flex flex-col gap-4 p-4 text-white">
           <a className="hover:text-[#EF3672]">Home</a>
-          <a className="hover:text-[#EF3672]">Your Activity</a>
-          <a className="hover:text-[#EF3672]">Saved</a>
+          <select
+            name="activity"
+            id="activity"
+            className="hover:text-[#EF3672] text-white outline-none bg-transparent"
+          >
+            <option value="activity" className="text-white bg-black">
+              Your Activity
+            </option>
+            <option value="activity" className="text-white bg-black">
+              Saab
+            </option>
+            <option value="activity" className="text-white bg-black">
+              Mercedes
+            </option>
+            <option value="activity" className="text-white bg-black">
+              Audi
+            </option>
+          </select>
+          <select
+            name="saved"
+            id="saved"
+            className="hover:text-[#EF3672] text-white  outline-none bg-black"
+          >
+            <option value="activity" className="text-white bg-black">
+              Saved
+            </option>
+            <option value="activity" className="text-white bg-black">
+              Saab
+            </option>
+            <option value="activity" className="text-white bg-black">
+              Mercedes
+            </option>
+            <option value="activity" className="text-white bg-black">
+              Audi
+            </option>
+          </select>
           <a className="hover:text-[#EF3672]">Favorites</a>
           <a className="hover:text-[#EF3672]">Group Profiles</a>
 
-          <div className="mt-6 p-[4px] rounded-4xl bg-gradient-to-br from-[rgba(252,114,146,0.4)] to-[rgba(238,237,237,0.2)] cursor-pointer">
-            <div className="text-[13px] font-[500] rounded-4xl bg-gradient-to-br from-[rgba(252,114,146,0.4)] to-[rgba(238,237,237,0.2)] py-[9px] px-[10px] text-black flex items-center gap-[7px]">
+          <div className="mt-6 p-[4px] rounded-4xl bg-gradient-to-br from-[rgba(252,114,146,0.4)] to-[rgba(238,237,237,0.2)] cursor-pointer w-[51%]">
+            <div className="text-[13px] font-[500] rounded-4xl bg-gradient-to-br from-[rgba(252,114,146,0.4)] to-[rgba(238,237,237,0.2)] py-[9px] px-[10px] text-white flex items-center gap-[7px]">
               Get App
               <div className="flex items-center gap-[7px]">
-                <Image src={appstore} alt="appstorelink" className="w-[12px] h-[15px]" />
-                <Image src={playstore} alt="playstorelink" className="w-[12px] h-[15px]" />
+                <Image
+                  src={appstore}
+                  alt="appstorelink"
+                  className="w-[12px] h-[15px]"
+                />
+                <Image
+                  src={playstore}
+                  alt="playstorelink"
+                  className="w-[12px] h-[15px]"
+                />
               </div>
             </div>
           </div>
         </ul>
       </div>
-      {isOpen && (
-        <div
-          onClick={() => setIsOpen(false)}
-          className="fixed inset-0 bg-black bg-opacity-50 z-40 lg:hidden"
-        ></div>
-      )}
     </>
   );
 }
